05e7fab20e227f4da8284a6c45677a558e729f4c
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-06-11 10:14:19 +0200

    Fix factor/character coercing warning

--- pareg/R/utils.R
+++ pareg/R/utils.R
@@ -2,12 +2,12 @@
 #'
 #' Store term membership for each gene
 create_model_df <- function (df.genes, df.terms) {
-  # TODO: fix factor/character warning
   df.model <- df.terms %>%
     group_by(name) %>%
     mutate(member=gene %in% df.genes$gene) %>%
     ungroup %>%
-    right_join(df.genes, by="gene") %>%
+    mutate_at(vars(gene), as.character) %>%
+    right_join(df.genes %>% mutate_at(vars(gene), as.character), by="gene") %>%
     spread(key=name, value=member, fill=FALSE) %>%
     select(-one_of("<NA>")) %>% # use `one_of` to handle case where <NA> does not exist
     rename_at(vars(-gene, -pvalue), ~ paste0(., ".member")) %>%
