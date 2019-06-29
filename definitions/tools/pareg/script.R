725cd9f952dd8dc357ad32d111d495ee7243d3d5
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-06-29 12:24:18 +0200

    Fix "invalid dependent variable" error

--- definitions/tools/pareg/script.R
+++ definitions/tools/pareg/script.R
@@ -4,6 +4,9 @@ library(pareg)
 df.genes = read.csv("genes.csv")
 df.terms = read.csv("terms.csv")
 
+# fix for "invalid dependent variable, all observations must be in (0, 1)"
+df.genes[df.genes$pvalue == 1, "pvalue"] <- 1 - 1e-7
+
 # run model
 df.enr <- pareg::pareg(df.genes, df.terms)
 
