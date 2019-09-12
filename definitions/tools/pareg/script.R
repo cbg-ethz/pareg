25a06617c25e144718881c3a36ccbaeb34d1a35c
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-09-12 16:03:49 +0200

    Response truncation is now part of model

--- definitions/tools/pareg/script.R
+++ definitions/tools/pareg/script.R
@@ -4,10 +4,7 @@ library(pareg)
 df.genes = read.csv("genes.csv")
 df.terms = read.csv("terms.csv")
 
-# fix for "invalid dependent variable, all observations must be in (0, 1)"
-df.genes[df.genes$pvalue == 1, "pvalue"] <- 1 - 1e-7
-
 # run model
-df.enr <- pareg::pareg(df.genes, df.terms)
+df.enr <- pareg::pareg(df.genes, df.terms, truncate.response=TRUE)
 
 write.csv(df.enr, file="enrichment_result.csv", row.names=FALSE)
