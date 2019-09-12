f183a0a938b424bfea5e74e8ac7e781828106357
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-09-12 16:15:57 +0200

    Minor change ('=' -> '<-')

--- definitions/tools/pareg/script.R
+++ definitions/tools/pareg/script.R
@@ -1,8 +1,8 @@
 library(pareg)
 
 # setup data
-df.genes = read.csv("genes.csv")
-df.terms = read.csv("terms.csv")
+df.genes <- read.csv("genes.csv")
+df.terms <- read.csv("terms.csv")
 
 # run model
 df.enr <- pareg::pareg(df.genes, df.terms, truncate.response=TRUE)
