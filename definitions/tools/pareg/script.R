460dd9ad8a664e46021a91dbf1e4326d9c84e2a6
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-05-27 12:54:39 +0200

    Readd accidentally deleted script

---
+++ definitions/tools/pareg/script.R
@@ -0,0 +1,10 @@
+library(pareg)
+
+# setup data
+df.genes <- read.csv("genes.csv")
+df.terms <- read.csv("terms.csv")
+
+# run model
+df.enr <- pareg::pareg(df.genes, df.terms, truncate.response=TRUE)
+
+write.csv(df.enr, file="enrichment_result.csv", row.names=FALSE)
