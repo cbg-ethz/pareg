7c30fb3af7a2f4c4f7520bffe2e6f631709dc401
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-08-25 11:34:22 +0200

    Add basic integration test

---
+++ pareg/tests/testthat/test-pareg.R
@@ -0,0 +1,29 @@
+test_that("package doesn't crash for trivial case", {
+  # create synthetic data
+  set.seed(42)
+
+  df.genes <- data.frame(
+    gene=paste("g", 1:20, sep=""),
+    pvalue=c(
+      rbeta(10, .1, 1),
+      rbeta(10, 1.1, 1)
+    )
+  )
+
+  df.terms <- rbind(
+    data.frame(
+      name="foo",
+      gene=paste("g", 1:10, sep="")
+    ),
+    data.frame(
+      name="bar",
+      gene=paste("g", 11:20, sep="")
+    )
+  )
+
+  # run model
+  res <- pareg(df.genes, df.terms)
+
+  # check results
+  expect_lt(res %>% filter(name == "foo") %>% pull(enrichment), res %>% filter(name == "bar") %>% pull(enrichment))
+})
