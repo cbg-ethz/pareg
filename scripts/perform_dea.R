f760d82c232c0049ef5a4c6178fdacec1f32d115
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-09-07 10:46:55 +0200

    Lint DEA script

--- scripts/perform_dea.R
+++ scripts/perform_dea.R
@@ -2,20 +2,20 @@ library(tidyverse)
 
 
 # read data
-df.cts <- read_csv(snakemake@input$expr_fname) %>% column_to_rownames("node")
-df.info <- read_csv(snakemake@input$info_fname) %>% column_to_rownames("sample")
+df_cts <- read_csv(snakemake@input$expr_fname) %>% column_to_rownames("node")
+df_info <- read_csv(snakemake@input$info_fname) %>% column_to_rownames("sample")
 
 # do DEA
 dds <- DESeq2::DESeqDataSetFromMatrix(
-  countData=df.cts,
-  colData=df.info,
-  design=~condition
+  countData = df_cts,
+  colData = df_info,
+  design = ~ condition
 )
 dds <- DESeq2::DESeq(dds)
-df.res <- DESeq2::results(dds)
+df_res <- DESeq2::results(dds)
 
 # store output
-df.res %>%
+df_res %>%
   as.data.frame %>%
   rownames_to_column("node") %>%
   arrange(padj) %>%
