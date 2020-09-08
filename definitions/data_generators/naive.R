c6f9f660783044084d0eb035a4f113facd96e4bf
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-09-08 12:29:00 +0200

    More flexible data generation

--- definitions/data_generators/naive.R
+++ definitions/data_generators/naive.R
@@ -2,50 +2,81 @@ library(tidyverse)
 
 
 # parameters
-node_num <- 100 # rows
+node_num <- 10 # rows, per pathway
+pathway_num <- 3 # per category
+
 sample_num <- 100 # columns
 
-# create dummy pathway topology
-cell_wt <- matrix(0, node_num, node_num)
+# helper functions
+generate_count_matrix <- function(
+  pathway_num, node_num, count_mean,
+  perturbation_parameter, pathway_name_template
+) {
+  purrr::map_dfr(seq_len(pathway_num), function(i) {
+    condition_size <- round(sample_num / 2)
+
+    # compute wildtype counts
+    wt_block <- matrix(
+      rnbinom(
+        node_num * condition_size,
+        size = 100, mu = count_mean
+      ),
+      node_num, condition_size
+    ) %>%
+      as.data.frame
+    colnames(wt_block) <- paste0("WT_", seq_len(condition_size))
+
+    # compute mutant counts
+    perturbation_vector <- if_else(
+      runif(node_num * condition_size) > perturbation_parameter,
+      3, 1
+    )
+    mt_block <- matrix(
+      rnbinom(
+        node_num * condition_size,
+        size = 100, mu = count_mean * perturbation_vector
+      ),
+      node_num, condition_size
+    ) %>%
+      as.data.frame
+    colnames(mt_block) <- paste0("MT_", seq_len(condition_size))
+
+    # concat count blocks
+    df_cts <- bind_cols(wt_block, mt_block)
+    rownames(df_cts) <- paste0(
+      glue::glue(pathway_name_template), seq_len(node_num)
+    )
+
+    df_cts
+  })
+}
 
 # generate count data
 count_mean <- 1000
 
-df_cts <- matrix(
-  rnbinom(node_num * sample_num, size = 100, mu = count_mean),
-  node_num, sample_num
-) %>%
-  as.data.frame
+df_cts <- bind_rows(
+  generate_count_matrix(
+    pathway_num, node_num, count_mean,
+    .75, "pw{i}weak_node"
+  ),
+  generate_count_matrix(
+    pathway_num, node_num, count_mean,
+    .5, "pw{i}medium_node"
+  ),
+  generate_count_matrix(
+    pathway_num, node_num, count_mean,
+    .25, "pw{i}strong_node"
+  ),
 
-rownames(df_cts) <- paste0("node_", 1:node_num)
-colnames(df_cts) <- c(paste0("WT_", 1:50), paste0("MT_", 1:50))
-
-# simulate pathway enrichment
-condition_size <- round(sample_num / 2)
-wt_range <- seq(condition_size + 1, sample_num)
-
-df_cts[1:10, wt_range] <- rnbinom(
-  10 * condition_size,
-  size = 100, mu = count_mean * if_else(runif(100) > .75, 3, 1)
-)
-df_cts[11:20, wt_range] <- rnbinom(
-  10 * condition_size,
-  size = 100, mu = count_mean * if_else(runif(100) > .5, 3, 1)
-)
-df_cts[21:30, wt_range] <- rnbinom(
-  10 * condition_size,
-  size = 100, mu = count_mean * if_else(runif(100) > .25, 3, 1)
+  generate_count_matrix(
+    1, 500, count_mean,
+    1, "bg{i}_node"
+  )
 )
 
-rownames(df_cts) <- c(
-  paste0("pw0weak_", rownames(df_cts[1:10, ])),
-  paste0("pw0medium_", rownames(df_cts[11:20, ])),
-  paste0("pw0strong_", rownames(df_cts[21:30, ])),
-  paste0("bg_", rownames(df_cts[31:node_num, ]))
-)
-
-rownames(cell_wt) <- rownames(df_cts)
-colnames(cell_wt) <- rownames(df_cts)
+# create dummy pathway topology
+cell_wt <- matrix(0, dim(df_cts)[[1]], dim(df_cts)[[1]])
+rownames(cell_wt) <- colnames(cell_wt) <- rownames(df_cts)
 
 # store sample information
 df_info <- data.frame(rowname = df_cts %>% colnames) %>%
