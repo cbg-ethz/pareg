32d6fa4865e614026fcc732006c2946a0c8f5e50
kpj <kim.philipp.jablonski@gmail.com>
2022-01-17 23:31:01 +0100

    Add pareg with poisson response distribution to benchmark

---
+++ inst/scripts/resources/method_definitions/pareg_pois.R
@@ -0,0 +1,39 @@
+# prepare environment
+source(snakemake@params$setup_code_fname)
+
+devtools::load_all("../..")
+
+# run model
+fit <- pareg::pareg(
+  study$df %>% select(gene, pvalue),
+  df_terms,
+  family = netReg::poisson,
+  response_column_name = "pvalue_notsig"
+)
+
+df <- fit %>%
+  as.data.frame() %>%
+  mutate(method = "pareg_pois", enrichment = abs(enrichment))
+
+df %>%
+  arrange(desc(abs(enrichment))) %>%
+  head()
+
+# misc
+data.frame(
+  iteration = seq_along(fit$obj$loss_hist),
+  loss = fit$obj$loss_hist
+) %>%
+  ggplot(aes(x = iteration, y = loss)) +
+    geom_line() +
+    geom_point() +
+    theme_minimal()
+ggsave(
+  file.path(dirname(snakemake@output$fname), "loss.pdf"),
+  width = 8,
+  height = 6
+)
+
+# save result
+df %>%
+  write_csv(snakemake@output$fname)
