2e4ae0725188c5d4c382dddd6c6cb17c28664750
kpj <kim.philipp.jablonski@gmail.com>
2022-01-17 23:24:24 +0100

    Plot loss history for pareg methods in benchmark

--- inst/scripts/resources/method_definitions/pareg_network_cv.R
+++ inst/scripts/resources/method_definitions/pareg_network_cv.R
@@ -4,19 +4,36 @@ source(snakemake@params$setup_code_fname)
 devtools::load_all("../..")
 
 # run model
-df <- pareg::pareg(
+fit <- pareg::pareg(
   study$df %>% select(gene, pvalue),
   df_terms,
   term_network = term_similarities_sub,
   truncate_response = TRUE,
   cv = TRUE
-) %>%
+)
+
+df <- fit %>%
   as.data.frame() %>%
   mutate(method = "pareg_network_cv", enrichment = abs(enrichment))
 
 df %>%
   arrange(desc(abs(enrichment))) %>%
   head()
+
+# misc
+data.frame(
+  iteration = seq_along(fit$obj$loss_hist),
+  loss = fit$obj$loss_hist
+) %>%
+  ggplot(aes(x = iteration, y = loss)) +
+    geom_line() +
+    geom_point() +
+    theme_minimal()
+ggsave(
+  file.path(dirname(snakemake@output$fname), "loss.pdf"),
+  width = 8,
+  height = 6
+)
 
 # save result
 df %>%
