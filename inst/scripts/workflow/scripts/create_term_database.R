5bb100ea65501a561dbefbce52daaff761418443
kpj <kim.philipp.jablonski@gmail.com>
2022-01-21 11:03:36 +0100

    Make term source specification more flexible

--- inst/scripts/workflow/scripts/create_term_database.R
+++ inst/scripts/workflow/scripts/create_term_database.R
@@ -7,93 +7,90 @@ library(msigdbr)
 fname_out <- snakemake@output$fname
 
 term_filter_params <- snakemake@params$term_filter
-category <- snakemake@params$params$category
-subcategory <- snakemake@params$params$subcategory
+termsource <- snakemake@params$params$termsource
 
-# overview (http://www.gsea-msigdb.org/gsea/msigdb/collections.jsp)
-msigdbr_species()
-msigdbr_collections() %>%
-  arrange(desc(num_genesets))
-msigdbr_collections() %>%
-  group_by(gs_cat) %>%
-  summarize(num_genesets = sum(num_genesets)) %>%
-  arrange(desc(num_genesets))
+parts <- strsplit(termsource, ";")[[1]]
 
-# retrieve terms
-df_terms <- msigdbr(species = "Homo sapiens")
+if (parts[[1]] == "msigdb") {
+  category <- parts[[2]]
+  subcategory <- parts[[3]]
 
-df_terms %>%
-  head()
+  # overview (http://www.gsea-msigdb.org/gsea/msigdb/collections.jsp)
+  msigdbr_species()
+  msigdbr_collections() %>%
+    arrange(desc(num_genesets))
+  msigdbr_collections() %>%
+    group_by(gs_cat) %>%
+    summarize(num_genesets = sum(num_genesets)) %>%
+    arrange(desc(num_genesets))
 
-df_terms %>%
-  group_by(gs_name) %>%
-  tally() %>%
-  arrange(desc(n))
+  # retrieve terms
+  df_terms <- msigdbr(species = "Homo sapiens")
+
+  # subset terms
+  df_terms <- df_terms %>%
+    filter(gs_cat == category) %>%
+    {
+      if (subcategory != "None") {
+        print("Filtering subcategory")
+        filter(., gs_subcat == subcategory)
+      } else {
+        print("Skipping subcategory filter")
+        .
+      }
+    } %>%
+    select(gs_name, gene_symbol) %>%
+    rename(term = gs_name, gene = gene_symbol) %>%
+    distinct(.keep_all = TRUE)
+} else {
+  stop(paste("Unknown term database source:", parts[[1]]))
+}
 
 # select terms of reasonable size
-gs_selection <- df_terms %>%
-  group_by(gs_cat, gs_name) %>%
+term_selection <- df_terms %>%
+  group_by(term) %>%
   tally() %>%
   arrange(desc(n))
 
 if (!is.null(term_filter_params$min_size)) {
   print("Filtering pathway by min_size")
-  gs_selection %<>%
+  term_selection %<>%
     filter(term_filter_params$min_size < n)
 }
 if (!is.null(term_filter_params$max_size)) {
   print("Filtering pathway by max_size")
-  gs_selection %<>%
+  term_selection %<>%
     filter(n < term_filter_params$max_size)
 }
 if (!is.null(term_filter_params$sample_num)) {
   print("Sampling pathways")
-  gs_selection %<>%
+  term_selection %<>%
     group_by(gs_cat) %>%
     sample_n(min(term_filter_params$sample_num, n())) %>%
     ungroup()
 }
 
-gs_selection %<>%
-  pull(gs_name)
-
-gs_selection %>%
+term_selection %<>%
+  pull(term)
+term_selection %>%
   head()
 
-df_sel <- df_terms %>%
-  filter(gs_name %in% gs_selection)
-
-df_sel %>%
-  head()
-
-df_sel %>%
-  distinct(gs_name) %>%
-  dim()
-
-# apply term source options
-df_sel <- df_sel %>%
-  filter(gs_cat == category) %>%
-  {
-    if (subcategory != "None") {
-      print("Filtering subcategory")
-      filter(., gs_subcat == subcategory)
-    } else {
-      print("Skipping subcategory filter")
-      .
-    }
-  } %>%
-  select(gs_name, gene_symbol) %>%
-  rename(term = gs_name, gene = gene_symbol) %>%
-  distinct(.keep_all = TRUE)
+df_sub <- df_terms %>%
+  filter(term %in% term_selection)
 
 # data overview
-df_sel %>%
+df_sub %>%
   head()
 
-df_sel %>%
+df_sub %>%
   distinct(term) %>%
   dim()
 
+df_sub %>%
+  group_by(term) %>%
+  tally() %>%
+  arrange(desc(n))
+
 # save result
-df_sel %>%
+df_sub %>%
   write_csv(fname_out)
