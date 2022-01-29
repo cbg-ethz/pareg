94b18402dcf5ca8171b07d2ea8045e5e4c8e5a26
kpj <kim.philipp.jablonski@gmail.com>
2022-01-29 11:37:31 +0100

    Add pareg method with network regularization and phi fitting

---
+++ inst/scripts/resources/method_definitions/pareg_network_phi.R
@@ -0,0 +1,41 @@
+# prepare environment
+source(snakemake@params$setup_code_fname)
+
+devtools::load_all("../..")
+
+# run model
+fit <- pareg::pareg(
+  study$df %>% select(gene, pvalue),
+  df_terms,
+  network_param = 1, term_network = term_similarities_sub,
+  truncate_response = TRUE,
+  family = netReg::beta_phi
+)
+
+df <- fit %>%
+  as.data.frame() %>%
+  mutate(method = "pareg_network_phi", enrichment = abs(enrichment))
+
+df %>%
+  arrange(desc(abs(enrichment))) %>%
+  head()
+
+# misc
+do.call(rbind, fit$obj$loss_hist) %>%
+  as_tibble() %>%
+  unnest(everything()) %>%
+  mutate(iteration = seq_along(fit$obj$loss_hist)) %>%
+  pivot_longer(-iteration) %>%
+  ggplot(aes(x = iteration, y = value, color = name)) +
+    geom_line() +
+    # geom_point() +
+    theme_minimal()
+ggsave(
+  file.path(dirname(snakemake@output$fname), "loss.pdf"),
+  width = 8,
+  height = 6
+)
+
+# save result
+df %>%
+  write_csv(snakemake@output$fname)
