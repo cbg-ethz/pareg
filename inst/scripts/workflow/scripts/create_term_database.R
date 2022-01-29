2a8e4caa0609429a3b779a2e9f8e08ebfd014e29
kpj <kim.philipp.jablonski@gmail.com>
2022-01-29 12:38:45 +0100

    Replace ';' with ':' as term generation parameter separator

--- inst/scripts/workflow/scripts/create_term_database.R
+++ inst/scripts/workflow/scripts/create_term_database.R
@@ -9,7 +9,7 @@ fname_out <- snakemake@output$fname
 term_filter_params <- snakemake@params$term_filter
 termsource <- snakemake@wildcards$termsource
 
-parts <- strsplit(termsource, ";")[[1]]
+parts <- strsplit(termsource, ":")[[1]]
 
 if (parts[[1]] == "msigdb") {
   category <- parts[[2]]
