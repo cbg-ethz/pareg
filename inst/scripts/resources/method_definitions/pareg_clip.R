515a62024245fc9133ed01873ec80703b416f7ea
kpj <kim.philipp.jablonski@gmail.com>
2022-02-02 12:45:07 +0100

    Also benchmark clipped pareg method

---
+++ inst/scripts/resources/method_definitions/pareg_clip.R
@@ -0,0 +1,41 @@
+# prepare environment
+source(snakemake@params$setup_code_fname)
+
+devtools::load_all("../..")
+
+# run model
+clip_thres <- 10^-10
+fit <- pareg::pareg(
+  study$df %>%
+    select(gene, pvalue) %>%
+    mutate(pvalue = ifelse(pvalue <= clip_thres,  clip_thres, pvalue)),
+  df_terms
+)
+
+df <- fit %>%
+  as.data.frame() %>%
+  mutate(method = "pareg_clip", enrichment = abs(enrichment))
+
+df %>%
+  arrange(desc(abs(enrichment))) %>%
+  head()
+
+# misc
+do.call(rbind, fit$obj$loss_hist) %>%
+  as_tibble() %>%
+  unnest(everything()) %>%
+  mutate(iteration = seq_along(fit$obj$loss_hist)) %>%
+  pivot_longer(-iteration) %>%
+  ggplot(aes(x = iteration, y = value, color = name)) +
+    geom_line() +
+    # geom_point() +
+    theme_minimal()
+ggsave(
+  file.path(dirname(snakemake@output$fname), "loss.pdf"),
+  width = 8,
+  height = 6
+)
+
+# save result
+df %>%
+  write_csv(snakemake@output$fname)
