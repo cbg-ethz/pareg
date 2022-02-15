60456d76ca0bd952df6ef245db5560f14cce3078
kpj <kim.philipp.jablonski@gmail.com>
2022-02-15 10:48:17 +0100

    Fix test-utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,7 @@
 test_that("model creation works with only common genes", {
   df_genes <- data.frame(
     gene = c("g1", "g2"),
-    pvalue = c(0.1, 0.2)
+    pvalue = c(0.01, 0.2)
   )
   df_terms <- data.frame(
     term = c("A", "A", "B", "B", "C"),
@@ -11,10 +11,12 @@ test_that("model creation works with only common genes", {
   df_res <- create_model_df(df_genes, df_terms)
   df_expected <- tibble(
     gene = as.factor(c("g1", "g2")),
-    pvalue = c(0.1, 0.2),
+    pvalue = c(0.01, 0.2),
     A.member = c(TRUE, TRUE),
     B.member = c(TRUE, TRUE),
-    C.member = c(FALSE, TRUE)
+    C.member = c(FALSE, TRUE),
+    pvalue_sig = c(TRUE, FALSE),
+    pvalue_notsig = c(FALSE, TRUE)
   )
 
   expect_equal(df_res, df_expected)
@@ -23,7 +25,7 @@ test_that("model creation works with only common genes", {
 test_that("model creation works with gene which is in no term", {
   df_genes <- data.frame(
     gene = c("g1", "g2", "g3"),
-    pvalue = c(0.1, 0.2, 0.3)
+    pvalue = c(0.01, 0.2, 0.3)
   )
   df_terms <- data.frame(
     term = c("A", "A", "B", "B", "C"),
@@ -33,10 +35,12 @@ test_that("model creation works with gene which is in no term", {
   df_res <- create_model_df(df_genes, df_terms)
   df_expected <- tibble(
     gene = as.factor(c("g1", "g2", "g3")),
-    pvalue = c(0.1, 0.2, 0.3),
+    pvalue = c(0.01, 0.2, 0.3),
     A.member = c(TRUE, TRUE, FALSE),
     B.member = c(TRUE, TRUE, FALSE),
-    C.member = c(FALSE, TRUE, FALSE)
+    C.member = c(FALSE, TRUE, FALSE),
+    pvalue_sig = c(TRUE, FALSE, FALSE),
+    pvalue_notsig = c(FALSE, TRUE, TRUE)
   )
 
   expect_equal(df_res, df_expected)
