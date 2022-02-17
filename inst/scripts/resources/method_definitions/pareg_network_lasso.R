e221fa03249aded107f84c0597f56cdcf8779f4f
kpj <kim.philipp.jablonski@gmail.com>
2022-02-17 11:33:52 +0100

    Also test LASSO regularization

---
+++ inst/scripts/resources/method_definitions/pareg_network_lasso.R
@@ -0,0 +1,50 @@
+# prepare environment
+source(snakemake@params$setup_code_fname)
+
+devtools::load_all("../..")
+
+# run model
+fit <- pareg::pareg(
+  study$df %>% select(gene, pvalue),
+  df_terms,
+  lasso_param = 1,
+  network_param = 1, term_network = term_similarities_sub
+)
+
+df <- fit %>%
+  as.data.frame() %>%
+  mutate(method = "pareg_network_lasso", enrichment = abs(enrichment))
+
+df %>%
+  arrange(desc(abs(enrichment))) %>%
+  head()
+
+# misc
+df_loss <- do.call(rbind, fit$obj$loss_hist) %>%
+  as_tibble() %>%
+  unnest(everything()) %>%
+  mutate(iteration = seq_along(fit$obj$loss_hist))
+df_loss %>%
+  write_csv(file.path(dirname(snakemake@output$fname), "loss.csv"))
+
+df_loss %>%
+  pivot_longer(-iteration) %>%
+  ggplot(aes(x = iteration, y = value, color = name)) +
+    geom_line() +
+    # geom_point() +
+    theme_minimal()
+ggsave(
+  file.path(dirname(snakemake@output$fname), "loss.pdf"),
+  width = 8,
+  height = 6
+)
+
+data.frame(
+  pseudo_r_squared = fit$obj$pseudo_r_squared,
+  mse = fit$obj$mse
+) %>%
+  write_csv(file.path(dirname(snakemake@output$fname), "extra_stats.csv"))
+
+# save result
+df %>%
+  write_csv(snakemake@output$fname)
