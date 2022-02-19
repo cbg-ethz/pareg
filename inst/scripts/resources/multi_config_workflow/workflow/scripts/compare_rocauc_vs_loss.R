78dee029577a743e69a6da5ccf53f87ac1486213
kpj <kim.philipp.jablonski@gmail.com>
2022-02-19 14:38:27 +0100

    Compute loss in cross-validation precursor on test samples

--- inst/scripts/resources/multi_config_workflow/workflow/scripts/compare_rocauc_vs_loss.R
+++ inst/scripts/resources/multi_config_workflow/workflow/scripts/compare_rocauc_vs_loss.R
@@ -1,14 +1,69 @@
 library(tidyverse)
+devtools::load_all()
 
 
 # parameters
 fname_aucs <- snakemake@input$fname_aucs
 fname_enr_list <- snakemake@input$fname_enr_list
+fname_study_list <- snakemake@input$fname_study_list
+fname_method_list <- snakemake@input$fname_method_list
 
 outdir <- snakemake@output$outdir
 dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
 
 # read loss data
+df_loss_test <- pmap_dfr(
+  list(fname_enr_list, fname_study_list, fname_method_list),
+  function(path_enr, path_study, path_method) {
+    # parse parameters
+    path_parts <- gtools::split_path(path_enr, depth_first = FALSE)
+    param_str <- path_parts[[length(path_parts) - 4]]
+    tmp <- list()
+    for (param_pair in strsplit(param_str, "_")[[1]]) {
+      parts <- strsplit(param_pair, "~")[[1]]
+      tmp[parts[[1]]] <- parts[[2]]
+    }
+
+    replicate <- path_parts[[length(path_parts) - 2]]
+    method <- path_parts[[length(path_parts) - 1]]
+
+    if (str_detect(method, "pareg", negate = TRUE)) {
+      return(NULL)
+    }
+
+    # read data
+    fit <- readRDS(file.path(dirname(path_method), "fit.rds"))
+    mod <- keras::load_model_tf(fit$obj$model)
+    study <- readRDS(path_study)
+
+    # setup model inputs
+    df_model <- pareg::create_model_df(study$df %>% select(gene, pvalue), fit$df_terms)
+    covariates <- df_model %>%
+      select(ends_with(".member")) %>%
+      names()
+    X_test <- df_model %>%
+      select(all_of(covariates)) %>%
+      as.matrix()
+    Y_test <- df_model %>%
+      select(fit$response_column_name) %>%
+      as.matrix()
+
+    # compute loss scores on test samples
+    loss_test <- edgenet.loss(
+      fit$params$lasso_param, fit$params$network_param, 0,
+      cast_float(fit$term_network), NULL,
+      fit$obj$family
+    )(mod, cast_float(X_test), cast_float(transform_y(Y_test)))
+
+    # finalize result
+    data.frame(likelihood_test = loss_test$likelihood) %>%
+      rowwise %>%
+      mutate(
+        replicate = replicate,
+        !!!tmp
+      )
+})
+
 df_loss <- fname_enr_list %>%
   map_dfr(function(path_enr) {
     # parse parameters
@@ -63,7 +118,8 @@ df_aucs <- read_csv(
 
 # combine data sources
 df_res <- df_loss %>%
-  inner_join(df_aucs)
+  inner_join(df_aucs) %>%
+  inner_join(df_loss_test)
 
 df_res %>%
   head()
@@ -71,6 +127,10 @@ df_res %>%
 # create summary plot
 p <- cowplot::plot_grid(
   ggplot(df_res, aes(x = method, y = likelihood)) +
+    geom_boxplot() +
+    scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+    theme_minimal(),
+  ggplot(df_res, aes(x = method, y = likelihood_test)) +
     geom_boxplot() +
     scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
     theme_minimal(),
