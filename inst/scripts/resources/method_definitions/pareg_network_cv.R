4e1704da04c1ad8c77792011c1eda5e7486f4722
kpj <kim.philipp.jablonski@gmail.com>
2022-02-21 15:15:38 +0100

    Save extra stats for pareg_network_cv

--- inst/scripts/resources/method_definitions/pareg_network_cv.R
+++ inst/scripts/resources/method_definitions/pareg_network_cv.R
@@ -19,6 +19,37 @@ df %>%
   arrange(desc(abs(enrichment))) %>%
   head()
 
+# misc
+fname_model <- file.path(dirname(snakemake@output$fname), "model")
+fit$obj$fit$model$save(fname_model)
+fit$obj$fit$model <- fname_model
+saveRDS(fit, file.path(dirname(snakemake@output$fname), "fit.rds"))
+
+df_loss <- do.call(rbind, fit$obj$fit$loss_hist) %>%
+  as_tibble() %>%
+  unnest(everything()) %>%
+  mutate(iteration = seq_along(fit$obj$fit$loss_hist))
+df_loss %>%
+  write_csv(file.path(dirname(snakemake@output$fname), "loss.csv"))
+
+df_loss %>%
+  pivot_longer(-iteration) %>%
+  ggplot(aes(x = iteration, y = value, color = name)) +
+    geom_line() +
+    # geom_point() +
+    theme_minimal()
+ggsave(
+  file.path(dirname(snakemake@output$fname), "loss.pdf"),
+  width = 8,
+  height = 6
+)
+
+data.frame(
+  pseudo_r_squared = fit$obj$fit$pseudo_r_squared,
+  mse = fit$obj$fit$mse
+) %>%
+  write_csv(file.path(dirname(snakemake@output$fname), "extra_stats.csv"))
+
 # save result
 df %>%
   write_csv(snakemake@output$fname)
