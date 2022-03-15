6abc9041bc66e7926b6fb2b6e2f5604982d18f81
kpj <kim.philipp.jablonski@gmail.com>
2022-03-15 13:31:01 +0100

    More deps

--- R/pareg-package.R
+++ R/pareg-package.R
@@ -1,3 +1,4 @@
 #' @importFrom tibble tibble
-#' @importFrom ggplot2 ggplot scale_colour_continuous
+#' @importFrom ggplot2 ggplot geom_boxplot scale_colour_continuous
+#' @importFrom ggplot2 theme_minimal
 NULL
