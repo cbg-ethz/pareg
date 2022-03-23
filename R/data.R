2b806a440829461581bb0291d7ed9f15df810d58
kpj <kim.philipp.jablonski@gmail.com>
2022-03-23 09:15:19 +0100

    Document data object

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Collection of pathway similarity matrices.
+#'
+#' Contains matrices for various pathway databases and similarity measures.
+#'
+#' @format A list of lists of matrices.
+#' \describe{
+#'   \itemize{
+#'     \item Pathway database 1
+#'     \itemize{
+#'       \item Similarity measure 1
+#'       \item Similarity measure 2
+#'       \item ...
+#'     }
+#'     \item Pathway database 2
+#'     \item ...
+#'   }
+#' }
+"pathway_similarities"
