fbc0a554c46231349e183059fc5161f79667f46f
kpj <kim.philipp.jablonski@gmail.com>
2022-03-28 13:43:27 +0200

    Benchmark response distribution with cross-validation

---
+++ inst/scripts/resources/method_definitions/pareg_beta.R
@@ -0,0 +1,30 @@
+# prepare environment
+source(snakemake@params$setup_code_fname)
+
+devtools::load_all("../..")
+
+# run model
+future::plan(future::multisession, worker = snakemake@threads)
+fit <- pareg::pareg(
+  study$df %>% select(gene, pvalue),
+  df_terms,
+  term_network = term_similarities_sub,
+  cv = TRUE,
+  family = pareg::beta
+)
+future::plan(future::sequential) # shut down workers
+
+df <- fit %>%
+  as.data.frame() %>%
+  mutate(method = "pareg_beta", enrichment = abs(enrichment))
+
+df %>%
+  arrange(desc(abs(enrichment))) %>%
+  head()
+
+# misc
+pareg_post_processing(fit, dirname(snakemake@output$fname))
+
+# save result
+df %>%
+  write_csv(snakemake@output$fname)
---
+++ inst/scripts/resources/method_definitions/pareg_gaussian.R
@@ -0,0 +1,36 @@
+# prepare environment
+source(snakemake@params$setup_code_fname)
+
+devtools::load_all("../..")
+
+# run model
+future::plan(future::multisession, worker = snakemake@threads)
+fit <- pareg::pareg(
+  study$df %>%
+    select(gene, pvalue) %>%
+    mutate(pvalue = -ifelse(
+      pvalue > 0,
+      log10(pvalue),
+      log10(min(pvalue[pvalue > 0]))
+    )),
+  df_terms,
+  term_network = term_similarities_sub,
+  cv = TRUE,
+  family = pareg::gaussian
+)
+future::plan(future::sequential) # shut down workers
+
+df <- fit %>%
+  as.data.frame() %>%
+  mutate(method = "pareg_gaussian", enrichment = abs(enrichment))
+
+df %>%
+  arrange(desc(abs(enrichment))) %>%
+  head()
+
+# misc
+pareg_post_processing(fit, dirname(snakemake@output$fname))
+
+# save result
+df %>%
+  write_csv(snakemake@output$fname)
