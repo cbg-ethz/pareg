ca878769535c47809161ca668a6e4e5f665e6432
kpj <kim.philipp.jablonski@gmail.com>
2022-04-05 14:56:15 +0200

    Add missing import

--- R/pareg-package.R
+++ R/pareg-package.R
@@ -1,4 +1,5 @@
 #' @importFrom tibble tibble
 #' @importFrom ggplot2 ggplot geom_boxplot scale_colour_continuous
+#' @importFrom ggplot2 scale_fill_continuous
 #' @importFrom ggplot2 theme_minimal
 NULL
