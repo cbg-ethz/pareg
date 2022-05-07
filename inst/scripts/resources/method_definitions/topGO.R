971a0bee1ef296a73355d36d1d7ef0c7d9168f6e
kpj <kim.philipp.jablonski@gmail.com>
2022-05-07 15:54:01 +0200

    Fix topGO implementation

--- inst/scripts/resources/method_definitions/topGO.R
+++ inst/scripts/resources/method_definitions/topGO.R
@@ -24,15 +24,11 @@ gene_list <- study$df %>%
   dplyr::select(gene, pvalue) %>%
   deframe()
 
-name_map <- msigdbr::msigdbr(species = "Homo sapiens", category = "C5", subcategory = subcategory) %>%
-  dplyr::select(gs_name, gs_exact_source) %>%
-  distinct(gs_exact_source, .keep_all = TRUE) %>%
-  mutate(gs_name = str_to_lower(gs_name))
-
-df_terms_goid <- name_map %>%
-  right_join(df_terms, by = c("gs_name" = "term"))
-df_terms_goid %>%
-  head
+tmp <- df_terms %>%
+  mutate(term = term %>% str_to_upper %>% str_replace("_", ":"))
+terms_gene2GO <- split(tmp$term, tmp$gene)
+terms_gene2GO %>%
+  head(1)
 
 gene_selector_func <- function(input_list) {
   return(input_list < 0.05)
@@ -44,7 +40,7 @@ topgo_obj <- new(
   allGenes = gene_list,
   geneSel = gene_selector_func,
   annot = annFUN.gene2GO,
-  gene2GO = split(df_terms_goid$gs_exact_source, df_terms_goid$gene)
+  gene2GO = terms_gene2GO
 )
 
 res_elim <- runTest(topgo_obj, algorithm = "elim", statistic = "ks")
@@ -52,10 +48,9 @@ res_elim <- runTest(topgo_obj, algorithm = "elim", statistic = "ks")
 df <- GenTable(topgo_obj, enrichment = res_elim) %>%
   as_tibble %>%
   dplyr::select(GO.ID, enrichment) %>%
-  left_join(name_map, by = c("GO.ID" = "gs_exact_source")) %>%
-  dplyr::rename(term = gs_name) %>%
-  dplyr::select(term, enrichment) %>%
-  mutate(method = "topGO", enrichment = -log10(as.numeric(enrichment)))
+  dplyr::rename(term = GO.ID) %>%
+  mutate(term = term %>% str_to_lower %>% str_replace(":", "_")) %>%
+  mutate(method = "topgo", enrichment = -log10(as.numeric(enrichment)))
 
 # there might be some NA terms because topGO uses its own ontology
 df %>%
