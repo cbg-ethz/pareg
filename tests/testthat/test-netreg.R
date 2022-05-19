e0a124af55ee64fc572d465f43bf475c24ab1b69
Kim <kim.philipp.jablonski@gmail.com>
2022-05-19 09:58:52 -0700

    Add parallelized CV test

--- tests/testthat/test-netreg.R
+++ tests/testthat/test-netreg.R
@@ -104,6 +104,38 @@ test_that("Cross-validation with network regularization works", {
 })
 
 
+test_that("Cross-validation with network regularization works in parallel", {
+  skip_on_bioc()
+
+  set.seed(42)
+  plan(multicore, workers = 2)
+  registerDoFuture()
+  log_threshold(TRACE)
+
+  alpha <- 2.3
+  beta <- c(1.7, -0.4, 6)
+
+  X <- matrix(rnorm(1000 * 3), nrow = 1000)
+  Y <- rnorm(1000, X %*% beta + alpha)
+
+  amat <- matrix(runif(3 * 3), 3, 3)
+
+  # small iteration count to reduce runtime
+  fit <- pareg::cv_edgenet(
+    X,
+    Y,
+    G.X = amat,
+    family = pareg::gaussian,
+    maxit = 10,
+    lambda_range = c(0, 1),
+    psigx_range = c(0, 5, 10)
+  )
+
+  # expect_equal(as.vector(coef(fit)), c(alpha, beta), tolerance = 0.02)
+  expect_equal(dim(coef(fit)), c(4, 1))
+})
+
+
 test_that("laplacian computation works", {
   mat_adj <- matrix(c(1, 0.5, 0, 0.5, 1, 0.8, 0, 0.8, 1), 3, 3)
   mat_lap <- compute_norm_laplacian(mat_adj)
