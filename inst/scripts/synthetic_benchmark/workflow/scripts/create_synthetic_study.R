c4239213f7d7f350be0e48131f37338f06aa0c2b
kpj <kim.philipp.jablonski@gmail.com>
2022-06-09 15:08:38 +0200

    Implement lm based synthetic data simulation method

--- inst/scripts/synthetic_benchmark/workflow/scripts/create_synthetic_study.R
+++ inst/scripts/synthetic_benchmark/workflow/scripts/create_synthetic_study.R
@@ -16,6 +16,7 @@ beta <- snakemake@params$params$beta # false negative rate
 similarity_factor <- snakemake@params$params$similarityfactor
 on_term_count <- snakemake@params$params$ontermcount
 sig_gene_scaling <- snakemake@params$params$siggenescaling
+model <- snakemake@params$params$model
 
 
 # setup
@@ -39,73 +40,161 @@ on_terms <- pareg::similarity_sample(
 
 on_terms <- unique(on_terms)
 
-# extract activated genes from activated terms
-study_genes_orig <- df_terms %>%
-  filter(term %in% on_terms) %>%
-  distinct(gene) %>%
-  pull(gene)
+# model DE p-values
+if (model == "mgsa") {
+  # extract activated genes from activated terms
+  study_genes_orig <- df_terms %>%
+    filter(term %in% on_terms) %>%
+    distinct(gene) %>%
+    pull(gene)
 
-# remove false negatives from activated genes
-fn_genes <- sample(seq_along(study_genes_orig), size = length(study_genes_orig) * beta)
-if (length(fn_genes) > 0) {
-  study_genes <- study_genes_orig[-fn_genes]
-} else {
-  study_genes <- study_genes_orig
-}
+  # remove false negatives from activated genes
+  fn_genes <- sample(seq_along(study_genes_orig), size = length(study_genes_orig) * beta)
+  if (length(fn_genes) > 0) {
+    study_genes <- study_genes_orig[-fn_genes]
+  } else {
+    study_genes <- study_genes_orig
+  }
 
-# add false positives to activated genes
-other_genes_orig <- df_terms %>%
-  filter(!(term %in% on_terms)) %>%
-  distinct(gene) %>%
-  pull(gene) %>%
-  setdiff(study_genes_orig)
-stopifnot(length(intersect(study_genes_orig, other_genes_orig)) == 0)
+  # add false positives to activated genes
+  other_genes_orig <- df_terms %>%
+    filter(!(term %in% on_terms)) %>%
+    distinct(gene) %>%
+    pull(gene) %>%
+    setdiff(study_genes_orig)
+  stopifnot(length(intersect(study_genes_orig, other_genes_orig)) == 0)
 
-fp_genes <- sample(seq_along(other_genes_orig), size = length(study_genes_orig) * alpha)
-study_genes <- c(study_genes, other_genes_orig[fp_genes])
+  fp_genes <- sample(seq_along(other_genes_orig), size = length(study_genes_orig) * alpha)
+  study_genes <- c(study_genes, other_genes_orig[fp_genes])
 
-# find inactive genes
-nonstudy_genes <- df_terms %>%
-  distinct(gene) %>%
-  filter(!(gene %in% study_genes)) %>%
-  pull(gene)
+  # find inactive genes
+  nonstudy_genes <- df_terms %>%
+    distinct(gene) %>%
+    filter(!(gene %in% study_genes)) %>%
+    pull(gene)
 
-# compute how many pathways each gene is a member of
-if (sig_gene_scaling == "membercount") {
-  member_count <- purrr::map_dfc(study_genes, function(current_gene) {
-    df_terms %>%
-      group_by(term) %>%
-      summarise("{current_gene}" := current_gene %in% gene) %>%
-      select({{ current_gene }})
-  }) %>%
-    summarize(across(everything(), sum)) %>%
-    t %>%
-    as.data.frame %>%
-    pull(V1)
+  # compute how many pathways each gene is a member of
+  if (sig_gene_scaling == "membercount") {
+    member_count <- purrr::map_dfc(study_genes, function(current_gene) {
+      df_terms %>%
+        group_by(term) %>%
+        summarise("{current_gene}" := current_gene %in% gene) %>%
+        select({{ current_gene }})
+    }) %>%
+      summarize(across(everything(), sum)) %>%
+      t %>%
+      as.data.frame %>%
+      pull(V1)
 
-  # +2 to have reasonable parameter with member_count=1
-  rbeta_shape1_param <- 2 ^ -(2 + member_count)
-} else {
-  rbeta_shape1_param <- as.numeric(sig_gene_scaling)
-}
+    # +2 to have reasonable parameter with member_count=1
+    rbeta_shape1_param <- 2 ^ -(2 + member_count)
+  } else {
+    rbeta_shape1_param <- as.numeric(sig_gene_scaling)
+  }
 
-# compute "DE" p-values for active and inactive genes
-study_pvalues <- rbeta(length(study_genes), rbeta_shape1_param, 1) # peak at 0
-nonstudy_pvalues <- rbeta(length(nonstudy_genes), 1, 1) # uniform
+  # compute "DE" p-values for active and inactive genes
+  study_pvalues <- rbeta(length(study_genes), rbeta_shape1_param, 1) # peak at 0
+  nonstudy_pvalues <- rbeta(length(nonstudy_genes), 1, 1) # uniform
 
-df <- data.frame(
-  gene = c(study_genes, nonstudy_genes),
-  pvalue = c(study_pvalues, nonstudy_pvalues),
-  in_study = c(
-    rep(TRUE, length(study_genes)),
-    rep(FALSE, length(nonstudy_genes))
-  ),
-  in_orig_study = c(
-    study_genes %in% study_genes_orig,
-    nonstudy_genes %in% study_genes_orig
+  # aggregate results
+  df <- data.frame(
+    gene = c(study_genes, nonstudy_genes),
+    pvalue = c(study_pvalues, nonstudy_pvalues),
+    in_study = c(
+      rep(TRUE, length(study_genes)),
+      rep(FALSE, length(nonstudy_genes))
+    ),
+    in_orig_study = c(
+      study_genes %in% study_genes_orig,
+      nonstudy_genes %in% study_genes_orig
+    )
   )
-)
 
+  # sanity checks
+  stopifnot(all(sort(unique(c(study_genes, nonstudy_genes))) == sort(unique(df_terms$gene))))
+} else if (model == "linear") {
+  # generate ground truth coefficient vector
+  terms <- df_terms %>%
+    distinct(term) %>%
+    pull(term)
+
+  term_vector <- rep(0, times = length(terms))
+  names(term_vector) <- terms
+
+  term_vector[on_terms] <- -1
+
+  # generate term-membership matrix
+  mat_X <- df_terms %>%
+    group_by(.data$term) %>%
+    mutate(member = TRUE) %>%
+    ungroup() %>%
+    mutate_at(vars(.data$gene), as.character) %>%
+    pivot_wider(
+      names_from = .data$term,
+      values_from = .data$member,
+      values_fill = FALSE
+    ) %>%
+    select(
+      # use `any_of` to handle case where NA does not exist
+      # (happens when a gene appears in no term)
+      -any_of("NA")
+    ) %>%
+    mutate_at(
+      vars(.data$gene),
+      factor # gene is character if select statement is executed
+    )
+
+  genes <- mat_X$gene
+  mat_X <- mat_X %>%
+    select(-gene)
+
+  mat_X <- mat_X %>%
+    mutate(across(everything(), as.numeric))
+
+  # introduce noise
+  for (term in on_terms) {
+    neg_matches <- which(mat_X[[term]] == 0)
+    pos_matches <- which(mat_X[[term]] == 1)
+
+    # false positives
+    fp_count <- as.integer(alpha * length(neg_matches))
+    fp_ind <- sample(neg_matches, fp_count, replace = FALSE)
+    mat_X[fp_ind, term] <- 1
+
+    # false negatives
+    fn_count <- as.integer(beta * length(pos_matches))
+    fn_ind <- sample(pos_matches, fn_count, replace = FALSE)
+    mat_X[fn_ind, term] <- 0
+  }
+
+  # simulate p-values
+  term_vector <- term_vector[colnames(mat_X)]
+
+  eta <- as.matrix(mat_X) %*% term_vector
+
+  linkinv <- pareg::beta_phi_var()$linkinv
+  mu <- linkinv(eta)$numpy()
+  phi <- 1
+  p_values <- rbeta(length(mu), mu * phi, (1 - mu) * phi)
+  p_values <- transform_y(p_values)
+  names(p_values) <- genes
+
+  # aggregate results
+  study_genes <- df_terms %>%
+    filter(term %in% on_terms) %>%
+    pull(gene)
+
+  df <- data.frame(
+    gene = names(p_values),
+    pvalue = unname(p_values),
+    in_study = names(p_values) %in% study_genes,
+    in_orig_study = names(p_values) %in% study_genes # degenerate case
+  )
+} else {
+  stop("Invalid model")
+}
+
+# summarize results
 table(df$pvalue <= 0.05, df$in_study, dnn = c("sig. p-value", "in study"))
 table(
   df$in_study,
@@ -120,9 +209,6 @@ data.frame(
   summary = paste(as.vector(summary(df$pvalue)), collapse = "__")
 ) %>%
   write_csv(file.path(plotdir, "pvalue_stats.csv"))
-
-# sanity checks
-stopifnot(all(sort(unique(c(study_genes, nonstudy_genes))) == sort(unique(df_terms$gene))))
 
 # overview plots
 ggplot(df, aes(x = pvalue)) +
