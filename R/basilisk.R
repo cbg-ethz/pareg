edb410ab965a859dca04cbb8cf0aff0d27d48d51
kpj <kim.philipp.jablonski@gmail.com>
2023-07-07 19:20:00 +0200

    Use anaconda channel with tensorflow v2.10.0

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,7 +2,8 @@ pareg_env <- basilisk::BasiliskEnvironment(
     envname = "pareg",
     pkgname = "pareg",
     packages = c(
-        "tensorflow==2.11.1",
+        "tensorflow==2.10.0",
         "tensorflow-probability==0.19.0"
-    )
+    ),
+    channels = c("anaconda")
 )
